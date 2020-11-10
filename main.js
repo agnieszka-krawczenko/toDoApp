b288d413d551be196fab4cd0e0b187966667ef9d
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2020-11-10 14:15:22 +0100

    Add tasksList array

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ const addInput = document.querySelector(".add");
 const button = document.querySelector("button");
 const searchInput = document.querySelector(".search");
 const ul = document.querySelector("ul");
+const taskNumber = document.querySelector('h3 span');
+
+const tasksList = [];
 
 const addTask = (e) => {
     e.preventDefault();
     const taskName = addInput.value;
     if (taskName === "") return;
-    const li = document.createElement("li");
-    li.innerHTML = `${taskName} <button>Usuń zadanie</button>`;
-    ul.appendChild(li);
+    const task = document.createElement("li");
+    task.innerHTML = `${taskName} <button>Usuń zadanie</button>`;
+    ul.appendChild(task);
+    tasksList.push(task);
     addInput.value = "";
+    taskNumber.textContent = tasksList.length;
 }
 
 button.addEventListener("click", addTask);
